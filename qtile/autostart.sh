41aea17f9328f727a8d1daaaa9c3db42d9410657
Sam170703dev <91561846+samuelvergaramartin@users.noreply.github.com>
2023-10-13 22:52:42 +0200

    Created autostart.sh

---
+++ qtile/autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#configuración del teclado en español
+
+setxkbmap es &
+
+#iconos del sistema
+
+udiskie -t &
+
+nm-applet &
+
+volumeicon &
+
+cbatticon -u 5 &
+
+nitrogen --restore &
+
+#configuración transparencia para las terminales
+
+picom &
